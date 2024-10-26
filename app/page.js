bce98e2843782b16b3148cf9772a4bc01c6ba721
SaddamTechie <saddamtechie@gmail.com>
2024-10-26 11:22:10 +0300

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,75 @@
-import Image from "next/image";
+'use client';
+import { useState } from "react";
+import Swal from "sweetalert2";
 
-export default function Home() {
+const App = () => {
+  const [formData,setFormData] = useState({
+    username:'',
+    email:'',
+    password:'',
+  });
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    Swal.fire({
+      title:'FORM SUBMITTED',
+      icon:'success',
+      text:'Data Submitted to the server',
+      confirmButtonText:'Ok',
+    })
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className=" h-screen flex w-full justify-center items-center bg-neutral-300">
+      <form action="#" onSubmit={handleSubmit} className="p-4 shadow-lg max-w-[800px] w-[90%] bg-white">
+        <p className="font-semibold text-2xl my-4 text-center text-orange-500">
+          SIGNUP TO AGRIBIX
+        </p>
+        <div>
+          <p>Username</p>
+          <input
+            type="text"
+            name="username"
+            className="h-[40px] p-[10px]  w-full"
+            placeholder="Enter username"
+            style={{
+              border: "1px solid #333",
+            }}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="mt-4">
+          <p>Email</p>
+          <input
+            type="email"
+            name="email"
+            className="h-[40px] p-[10px]  w-full"
+            placeholder="Enter email"
+            style={{
+              border: "1px solid #333",
+            }}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        </div>
+        <div className="mt-4">
+          <p>Password</p>
+          <input
+            type="password"
+            name="password"
+            className="h-[40px] p-[10px]  w-full"
+            placeholder="Enter Password"
+            style={{
+              border: "1px solid #333",
+            }}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        </div>
+        <button
+          type="submit"
+          className="rounded-md p-2 text-white bg-gradient-to-tr from-orange-500 to-orange-800 w-full mt-4"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          SIGNUP
+        </button>
+      </form>
     </div>
   );
-}
+};
+
+export default App;
